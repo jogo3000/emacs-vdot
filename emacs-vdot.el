6c209cda6e05b948ed5f3cbffa89d16f7c4b3d81
Matti Uusitalo <uusitalo.matti@gmail.com>
2020-12-12 19:31:21 +0200

    Rename functions to avoid name collisions

--- emacs-vdot.el
+++ emacs-vdot.el
@@ -4,7 +4,7 @@
 ;;; Based in the work described in Jack Daniels' book - Daniels' Running Formula.
 ;;; Code:
 
-(defun %VOmax (seconds)
+(defun vdot-%VOmax (seconds)
   "Return the percentage of VOmax a runner can sustain for the duration given in SECONDS."
   (let ((minutes (/ (float seconds) 60)))
     (+ 0.8
@@ -13,13 +13,13 @@
        (* 0.2989558
           (exp (* -0.1932605 minutes))))))
 
-(defun VO2 (speed)
+(defun vdot-VO2 (speed)
   "Return the oxygen consumption of a runner when running in a given SPEED."
   (+ -4.60
      (* 0.182258 speed)
      (* 0.000104 (expt speed 2))))
 
-(defun m-min (distance time)
+(defun vdot-m-min (distance time)
   "Return speed as meters / minutes.
 
 Gilbert's formulas use meters / minutes as units.  This function
@@ -28,25 +28,25 @@ TIME using these units."
 
   (/ distance (/ time 60)))
 
-(defun VDot  (race-distance race-duration)
+(defun vdot-VDot  (race-distance race-duration)
   "Calculate the pseudo VO2max, VDot based on a RACE-DISTANCE and RACE-DURATION."
 
-  (/ (VO2 (m-min race-distance race-duration))
-     (%VOmax race-duration)))
+  (/ (vdot-VO2 (vdot-m-min race-distance race-duration))
+     (vdot-%VOmax race-duration)))
 
-(defun VO2->velocity (vo2)
+(defun vdot-VO2->velocity (vo2)
   "Calculate theoretical velocity when runner consumes given amount of oxygen VO2."
   (+ 29.54
      (* 5.000663 vo2)
      (* -0.007546 (expt vo2 2))))
 
-(defun pace->velocity (pace)
+(defun vdot-pace->velocity (pace)
   "Convert PACE into velocity.
 
 Pace is seconds / 1000 m so..."
   (/ 1000 (/ (float pace) 60)))
 
-(defun velocity->pace (velocity)
+(defun vdot-velocity->pace (velocity)
   "Convert VELOCITY to pace.
 
 velocity is m / min so...
@@ -57,14 +57,14 @@ Unit is seconds per 1000 meters"
 
   (truncate (* 60 (/ 1000 (float velocity)))))
 
-(defun effort (multiplier vdot)
+(defun vdot-pace-at-effort (multiplier vdot)
   "Calculate pace at a MULTIPLIER of VDOT.
 
 Return value is seconds / 1000 meters."
 
   (let* ((part (* multiplier vdot))
-         (velocity (VO2->velocity part)))
-    (velocity->pace velocity)))
+         (velocity (vdot-VO2->velocity part)))
+    (vdot-velocity->pace velocity)))
 
 
 (provide 'emacs-vdot)
